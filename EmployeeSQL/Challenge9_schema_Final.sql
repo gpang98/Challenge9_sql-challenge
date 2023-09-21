cabf0420fb48c9b0941ab682bc914cb2fa2b2279
gpang98 <135190892+gpang98@users.noreply.github.com>
2023-09-21 10:04:24 +0800

    9th Commit

--- EmployeeSQL/Challenge9_schema_Final.sql
+++ EmployeeSQL/Challenge9_schema_Final.sql
@@ -2,14 +2,14 @@
 -- titles Table
 CREATE TABLE titles (
   title_id VARCHAR PRIMARY KEY,
-  title VARCHAR NOT NULL
+  title VARCHAR
 );
 
 
 -- departments Table
 CREATE TABLE departments (
   dept_no VARCHAR PRIMARY KEY,
-  dept_name VARCHAR NOT NULL
+  dept_name VARCHAR
 );
 
 
@@ -48,5 +48,5 @@ CREATE TABLE dept_emp (
 CREATE TABLE salaries (
   emp_no INTEGER NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-  salary INT NOT NULL
+  salary INT
 );
